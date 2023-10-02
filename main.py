69b019b601017b1ee74a6a454c4a71e11ab60c97
mehakvirdy <mvirdy@purdue.edu>
2023-10-01 23:54:25 -0400

    writes the transcript for the entire "business records -- management (11)" subject

--- main.py
+++ main.py
@@ -4,41 +4,43 @@ import urllib
 from urllib import request
 from bs4 import BeautifulSoup
 
-# stores url of whole subject
-subject_url = ("https://digitalcollections.library.miami.edu/digital/collection/asm0341/search/searchterm/business%20re"
-               "cords%20--%20management!pan%20american%20world%20airways%2C%20inc.!Manuals%20(instructional%20materials"
-               ")/field/subjec!subjec!genre/mode/exact!exact!exact/conn/and!and!all")
-subject_html = request.urlopen(subject_url).json()
+subject_url = "https://digitalcollections.library.miami.edu/digital/api/search/collection/asm0341/searchterm/business%20records%20--%20management!pan%20american%20world%20airways%2C%20inc.!Manuals%20(instructional%20materials)/field/subjec!subjec!genre/mode/exact!exact!exact/conn/and!and!all/maxRecords/100"
+subject_html = request.urlopen(subject_url).read()
 subject_json = json.loads(subject_html)
 
-print(subject_json)
+items = subject_json['items']
 
+pageIDS = []
 
-url = "https://digitalcollections.library.miami.edu/digital/api/collections/asm0341/items/110210/false"
-html = request.urlopen(url).read()
-soup = BeautifulSoup(html, 'html.parser')
-site_json = json.loads(soup.text)
-
-parent = site_json['parent']
-children = parent['children']
+for x in items:
+    pageIDS.append(int(x['itemId']))
 
 ids = []
 
-for x in children:
-    ids.append(x['id'])
+for x in pageIDS:
+    url = "https://digitalcollections.library.miami.edu/digital/api/collections/asm0341/items/%d/false" %x
+    html = request.urlopen(url).read()
+    site_json = json.loads(html)
 
-file = open("transcript.txt", "x")
-file.close()
+    parent = site_json['parent']
+    children = parent['children']
 
-file = open("transcript.txt", "w")
+    for y in children:
+        ids.append(y['id'])
 
-for x in ids:
-    URL = "https://digitalcollections.library.miami.edu/digital/api/collections/asm0341/items/%d/false" %x
-    newHTML = request.urlopen(URL).read()
-    newSoup = BeautifulSoup(newHTML, 'html.parser')
-    site_page = json.loads(newSoup.text)
+    file = open("transcript.txt", "a")
 
-    file.write(site_page['text'])
-    file.write("\n\n----------------------------------------------------------------------------------------\n\n")
+    for z in ids:
+        URL = "https://digitalcollections.library.miami.edu/digital/api/collections/asm0341/items/%d/false" %z
+        newHTML= request.urlopen(URL).read()
+        site_page = json.loads(newHTML)
+
+        binary = (site_page['text'].encode('ascii', errors='ignore'))
+        file.write(binary.decode())
+        file.write("\n\n----------------------------------------------------------------------------------------\n\n")
+
+    file.write("\n\n\n ---------------------------------------------DOCUMENT DIVIDER---------------------------------------------\n\n\n")
+
+    ids = []
 
 file.close()
